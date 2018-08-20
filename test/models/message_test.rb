d537dd33524352d8eae8f7d14f6d2601864ce026
David Poisson <zeraaiur@hotmail.com>
2018-08-20 15:25:59 -0400

    mailer

---
+++ test/models/message_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class MessageTest < ActiveSupport::TestCase
+   test 'should be valid when all attributes are set' do
+    attrs = {
+      name: 'stephen',
+      email: 'stephen@example.org',
+      body: 'kthnxbai'
+    }
+
+    msg = Message.new attrs
+    assert msg.valid?, 'should be valid'
+  end
+end
