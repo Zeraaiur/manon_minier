93517d8ba7a8ee2d342379e6333f40186e7b5ea1
David Poisson <zeraaiur@hotmail.com>
2018-10-05 21:44:53 -0400

    email test

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,16 @@
 class MessageMailer < ApplicationMailer
-
+  require 'mailgun'
   def contact(message)
     @contenu = message.contenu
+    mg_client = Mailgun::Client.new ENV['mailgun_secret_api_key']
 
+    message_params = {:from => message.courriel,
+      :to => ENV['david.poisson.pare@gmail.com'],
+      :subject => 'Contact provenant du site web',
+      :text => message.contenu}
+      mg_client.send_message ENV['mailgun_domain'], message_params
 
-    mail to: "david.poisson.pare@gmail.com", from: message.courriel
+      send
+    end
+
   end
-
-end
