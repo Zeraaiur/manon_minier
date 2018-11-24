af89cb49bddae19157242d5c5aae647479f70ec8
David Poisson <zeraaiur@hotmail.com>
2018-11-24 11:16:46 -0500

    reverse to gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,11 +102,11 @@ config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default :charset => "utf-8"
 
 config.action_mailer.smtp_settings = {
-address: "mail.privateemail.com",
-port: 465,
+address: "smtp.gmail.com",
+port: 587,
 domain: ENV["GMAIL_DOMAIN"],
 authentication: "plain",
-enable_starttls_auto: false,
+enable_starttls_auto: true,
 user_name: ENV["GMAIL_USERNAME"],
 password: ENV["GMAIL_PASSWORD"]
 }
